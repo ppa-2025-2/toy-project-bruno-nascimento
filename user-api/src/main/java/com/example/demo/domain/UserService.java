3c65fd276d4f94f25ff462255069620f6151ea49
Chipskein <abfn0905@gmail.com>
2025-10-19 14:16:45 -0300

    orm

--- user-api/src/main/java/com/example/demo/domain/UserService.java
+++ user-api/src/main/java/com/example/demo/domain/UserService.java
@@ -1,60 +1,109 @@
 package com.example.demo.domain;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import com.example.demo.controller.dto.NewUserDTO;
-import com.example.demo.domain.stereotype.Business;
+import com.example.demo.domain.exceptions.NotFoundException;
+import com.example.demo.repository.IslandRepository;
 import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.entity.Island;
 import com.example.demo.repository.entity.Profile;
 import com.example.demo.repository.entity.Role;
 import com.example.demo.repository.entity.User;
+import com.example.demo.repository.entity.Workstation;
 
+import jakarta.persistence.EntityManager;
 import jakarta.validation.Valid;
 
 // Spring -> possui um container de Injeção de Dependências
 
 // estereótipo
-@Business // Domain, DomainService, Service, UseCase
+@Service // Domain, DomainService, Service, UseCase
 @Validated
-public class UserBusiness {
+public class UserService {
 
+    private final IslandRepository islandRepository;
+    
+    @SuppressWarnings("unused")
+    private final EntityManager em;
+    
     private final BCryptPasswordEncoder passwordEncoder = 
         new BCryptPasswordEncoder();
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final Set<String> defaultRoles;
 
-    public UserBusiness(
+    public UserService(
+        IslandRepository islandRepository,
+        EntityManager em,
         UserRepository userRepository,
         RoleRepository roleRepository,
         @Value("${app.user.default.roles}")
         Set<String> defaultRoles
     ) {
+        this.islandRepository = islandRepository;
+        this.em = em;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.defaultRoles = defaultRoles;
     }
+
+    // Application Service, interface entre o serviço e o domínio
+    // Domain Service, ele é o próprio domínio
+
+    public void alocarWorkstationDisponivel(@NonNull Integer userId) {
+        
+        final var user = userRepository.findById(userId)
+            .orElseThrow(() -> new NotFoundException());
+
+        // POO: agregação vs composição (aggregation vs composition)
+        final var islands = islandRepository.findIslandWithAvailableWorkstations();
+
+        if (islands.isEmpty()) {
+            throw new IllegalStateException("Workstations not available");
+        }
+
+        // island 0 square 2/4
+        // island 1 triangular 2/3
+        // island 3 rectangular 1/6
+
+        // busca ilhas começando por uma ws livre, depois duas, ...
+        Island freeIsland = islands.getFirst(); 
+        for (int slots = 1; slots < Island.Disposition.CIRCULAR.getPlacements(); slots++) {
+            final int positions = slots;
+            var possibleIsland = islands.stream()
+                .filter(i -> i.getWorkstations().stream()
+                            .map(Workstation::getUser)
+                            .filter(Objects::nonNull)
+                            .count() == positions)
+                .findFirst();
+            if (possibleIsland.isPresent()) {
+                freeIsland = possibleIsland.get();
+                break;
+            }
+        }
+
+        // primeira workstation livre e seta o usuário
+        freeIsland.getWorkstations().stream()
+            .filter(ws -> ws.getUser() == null)
+            .findFirst()
+            .ifPresent(ws -> ws.setUser(user));
+
+        islandRepository.save(freeIsland);
+    }
     
     // cadastrar usuário é um use case (é uma feature)
     public void cadastrarUsuario(@Valid NewUserDTO newUser) {
-        // if (newUser.email() == null || newUser.password() == null) {
-        //     throw new IllegalArgumentException("Email e senha são obrigatórios");
-        // }
-
-        // if (newUser.email().isEmpty() || newUser.password().isEmpty()) {
-        //     throw new IllegalArgumentException("Email e senha não podem estar vazios");
-        // }
-
-        // if (!newUser.email().matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
-        //     throw new IllegalArgumentException("Email não é válido");
-        // }
-
+ 
         if (!newUser.password().matches("^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$")) {
             throw new IllegalArgumentException("A senha deve ter pelo menos 8 caracteres e conter pelo menos uma letra e um número");
         }
@@ -100,6 +149,12 @@ public class UserBusiness {
         user.setProfile(profile);
 
         userRepository.save(user); 
+        
+        // Unit of Work
+        // se não houvesse repositório, usaríamos o EntityManager
+        // em.persist(user);
+        // em.flush();
+        
     }
 
     private String generateHandle(String email) {
